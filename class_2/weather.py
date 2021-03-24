453dbe85c39e6f2a8400547d8c2b8f52807585af
Your Name <you@example.com>
2021-03-24 15:42:47 +0800

    8 commit

---
+++ class_2/weather.py
@@ -0,0 +1,70 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import time
+import shutil
+import os
+def getLastestFileName():
+    path = 'C:\\Users\\User\\Downloads'
+    # 
+    try:
+        fileName = max([f for f in os.listdir(path)], key=lambda x: os.path.getmtime(os.path.join(path, x)))
+    except:
+        fileName = max([f for f in os.listdir(path)], key=lambda x: os.path.getmtime(os.path.join(path, x)))  
+    return fileName
+
+def changeName(finalName):
+    # 偵測檔案是否下載完成，完成下載的檔名最後會是.csv
+    while True:
+        fileName = getLastestFileName()
+        if fileName.endswith('.csv'):
+            break
+    path = 'C:\\Users\\User\\Downloads'
+    # 更改檔名
+    shutil.move(f'{path}\\{fileName}', f'{path}\\{finalName}')
+
+browser = webdriver.Chrome('./class_2/chromedriver.exe')
+browser.get('https://e-service.cwb.gov.tw/HistoryDataQuery/')
+
+# 非同步式載入的網頁才要用while loop
+while True:
+    try:
+        # 點選北部地區 id = Button_North
+        browser.find_element_by_id('Button_North').click()
+        break
+    except:
+        pass
+
+time.sleep(2)
+
+for item in Select(browser.find_element_by_id('stationCounty')).options:
+    # Print(item.text)
+    if item.text.find('新北市') != -1:
+        # 選擇測站所在城市 id = stationCounty
+        Select(browser.find_element_by_id('stationCounty'))\
+            .select_by_visible_text(item.text)
+
+for item in Select(browser.find_element_by_id('station')).options:
+    # 選擇測站 id = station
+    Select(browser.find_element_by_id('station'))\
+            .select_by_visible_text(item.text)
+
+    # 時間欄中輸入值 id = datepicker
+    browser.find_element_by_id('datepicker').send_keys('2021-03-22')
+    # 按下查詢按鈕 id = doquery
+    browser.find_element_by_id('doquery').click()
+
+    # 切換視窗到最後一個
+    windows = browser.window_handles
+    browser.switch_to_window(windows[-1])
+    # 點選路徑為 xpath 的圖片
+    browser.find_element_by_xpath("//input[@src='images/downloadCSV_2.png']").click()
+    
+    # 關閉視窗
+    browser.close()
+    # 切換為原視窗
+    browser.switch_to_window(windows[0])
+
+    time.sleep(1)
+    changeName(f"{item.text}.csv")
+    break
+    
